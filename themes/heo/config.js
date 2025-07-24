df1ed015d3740a6aae3127dde2af977fae4d40c0
iishm <iishm@proton.me>
2025-07-24 13:18:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '团队成果', url: '/tag/团队成果' },
-  HEO_HERO_CATEGORY_2: { title: '动态展台', url: '/tag/动态展台' },
-  HEO_HERO_CATEGORY_3: { title: '云端实验室', url: '/tag/云端实验室' },
+  HEO_HERO_CATEGORY_1: { title: '团队成果', url: '/chengguo' },
+  HEO_HERO_CATEGORY_2: { title: '动态展台', url: '/zhantai' },
+  HEO_HERO_CATEGORY_3: { title: '云端实验室', url: 'https://cloud.danlab.xyz' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
